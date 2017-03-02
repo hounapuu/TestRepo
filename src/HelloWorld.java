a836cdbd49fb19d9672357b05e88986c679179a5
DanBondarenko <danxfer@gmail.com>
2017-03-02 17:32:05 +0200

    dan oli siin

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("goodbye world");
         System.out.println("Ära anna veel alla!");
+        System.out.println("dan oli siin");
     }
 }
